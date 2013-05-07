9ca2129ba60e6331478250783dee4fd950a5653e
Trey Dockendorf <treydock@gmail.com>
2013-05-07 13:12:21 -0500

    - Add comments to rpm_gpg_key as pointed out by puppet-lint tests

--- manifests/rpm_gpg_key.pp
+++ manifests/rpm_gpg_key.pp
@@ -1,3 +1,21 @@
+# Define epel::rpm_gpg_key
+#
+# Actions:
+#   Import a RPM gpg key
+#
+# Parameters:
+#
+# [*path*]
+#   Path of the RPM GPG key to import
+#
+# Reqiures:
+#   You should probably be on an Enterprise Linux variant. (Centos, RHEL, Scientific, Oracle, Ascendos, et al)
+#
+# Sample Usage:
+#  epel::rpm_gpg_key{ "EPEL-6":
+#    path => "/etc/pki/rpm-gpg/RPM-GPG-KEY-EPEL-6"
+#  }
+#
 define epel::rpm_gpg_key($path) {
   # Given the path to a key, see if it is imported, if not, import it
   exec {  "import-${name}":
