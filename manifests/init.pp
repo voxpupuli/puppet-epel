0531572708fb6f3bdfb6b745a3f125740de3231b
Riccardo Calixte <rcalixte@broadinstitute.org>
2016-06-02 12:58:25 -0400

    Add options to disable repo resources when desired

--- manifests/init.pp
+++ manifests/init.pp
@@ -17,6 +17,7 @@ class epel (
   $epel_proxy                             = $epel::params::epel_proxy,
   $epel_enabled                           = $epel::params::epel_enabled,
   $epel_gpgcheck                          = $epel::params::epel_gpgcheck,
+  $epel_managed                           = $epel::params::epel_managed,
   $epel_exclude                           = undef,
   $epel_includepkgs                       = undef,
   $epel_sslclientkey                      = undef,
@@ -27,6 +28,7 @@ class epel (
   $epel_testing_proxy                     = $epel::params::epel_testing_proxy,
   $epel_testing_enabled                   = $epel::params::epel_testing_enabled,
   $epel_testing_gpgcheck                  = $epel::params::epel_testing_gpgcheck,
+  $epel_testing_managed                   = $epel::params::epel_testing_managed,
   $epel_testing_exclude                   = undef,
   $epel_testing_includepkgs               = undef,
   $epel_testing_sslclientkey              = undef,
@@ -37,6 +39,7 @@ class epel (
   $epel_source_proxy                      = $epel::params::epel_source_proxy,
   $epel_source_enabled                    = $epel::params::epel_source_enabled,
   $epel_source_gpgcheck                   = $epel::params::epel_source_gpgcheck,
+  $epel_source_managed                    = $epel::params::epel_source_managed,
   $epel_source_exclude                    = undef,
   $epel_source_includepkgs                = undef,
   $epel_source_sslclientkey               = undef,
@@ -47,6 +50,7 @@ class epel (
   $epel_debuginfo_proxy                   = $epel::params::epel_debuginfo_proxy,
   $epel_debuginfo_enabled                 = $epel::params::epel_debuginfo_enabled,
   $epel_debuginfo_gpgcheck                = $epel::params::epel_debuginfo_gpgcheck,
+  $epel_debuginfo_managed                 = $epel::params::epel_debuginfo_managed,
   $epel_debuginfo_exclude                 = undef,
   $epel_debuginfo_includepkgs             = undef,
   $epel_debuginfo_sslclientkey            = undef,
@@ -57,6 +61,7 @@ class epel (
   $epel_testing_source_proxy              = $epel::params::epel_testing_source_proxy,
   $epel_testing_source_enabled            = $epel::params::epel_testing_source_enabled,
   $epel_testing_source_gpgcheck           = $epel::params::epel_testing_source_gpgcheck,
+  $epel_testing_source_managed            = $epel::params::epel_testing_source_managed,
   $epel_testing_source_exclude            = undef,
   $epel_testing_source_includepkgs        = undef,
   $epel_testing_source_sslclientkey       = undef,
@@ -67,14 +72,19 @@ class epel (
   $epel_testing_debuginfo_proxy           = $epel::params::epel_testing_debuginfo_proxy,
   $epel_testing_debuginfo_enabled         = $epel::params::epel_testing_debuginfo_enabled,
   $epel_testing_debuginfo_gpgcheck        = $epel::params::epel_testing_debuginfo_gpgcheck,
+  $epel_testing_debuginfo_managed         = $epel::params::epel_testing_debuginfo_managed,
   $epel_testing_debuginfo_exclude         = undef,
   $epel_testing_debuginfo_includepkgs     = undef,
   $epel_testing_debuginfo_sslclientkey    = undef,
   $epel_testing_debuginfo_sslclientcert   = undef,
+  $epel_gpg_managed                       = $epel::params::epel_gpg_managed,
   $os_maj_release                         = $epel::params::os_maj_release,
 ) inherits epel::params {
 
+  Epel::Rpm_gpg_key <| |> -> Yumrepo <| |>
+
   if "${::osfamily}" == 'RedHat' and "${::operatingsystem}" !~ /Fedora|Amazon/ { # lint:ignore:only_variable_string
+  if $epel_testing_managed {
     yumrepo { 'epel-testing':
       # lint:ignore:selector_inside_resource
       mirrorlist     => $epel_testing_baseurl ? {
@@ -94,7 +104,9 @@ class epel (
       sslclientkey   => $epel_testing_sslclientkey,
       sslclientcert  => $epel_testing_sslclientcert,
     }
+  }
 
+  if $epel_testing_debuginfo_managed {
     yumrepo { 'epel-testing-debuginfo':
       # lint:ignore:selector_inside_resource
       mirrorlist     => $epel_testing_debuginfo_baseurl ? {
@@ -114,7 +126,9 @@ class epel (
       sslclientkey   => $epel_testing_debuginfo_sslclientkey,
       sslclientcert  => $epel_testing_debuginfo_sslclientcert,
     }
+  }
 
+  if $epel_testing_source_managed {
     yumrepo { 'epel-testing-source':
       # lint:ignore:selector_inside_resource
       mirrorlist     => $epel_testing_source_baseurl ? {
@@ -134,7 +148,9 @@ class epel (
       sslclientkey   => $epel_testing_source_sslclientkey,
       sslclientcert  => $epel_testing_source_sslclientcert,
     }
+  }
 
+  if $epel_managed {
     yumrepo { 'epel':
       # lint:ignore:selector_inside_resource
       mirrorlist     => $epel_baseurl ? {
@@ -154,7 +170,9 @@ class epel (
       sslclientkey   => $epel_sslclientkey,
       sslclientcert  => $epel_sslclientcert,
     }
+  }
 
+  if $epel_debuginfo_managed {
     yumrepo { 'epel-debuginfo':
       # lint:ignore:selector_inside_resource
       mirrorlist     => $epel_debuginfo_baseurl ? {
@@ -174,7 +192,9 @@ class epel (
       sslclientkey   => $epel_debuginfo_sslclientkey,
       sslclientcert  => $epel_debuginfo_sslclientcert,
     }
+  }
 
+  if $epel_source_managed {
     yumrepo { 'epel-source':
       # lint:ignore:selector_inside_resource
       mirrorlist     => $epel_source_baseurl ? {
@@ -194,10 +214,11 @@ class epel (
       sslclientkey   => $epel_source_sslclientkey,
       sslclientcert  => $epel_source_sslclientcert,
     }
+  }
 
-    # ERB template used here to ensure file content is in the Puppet catalog;
-    # nothing is interpolated in these templates.
-
+  # ERB template used here to ensure file content is in the Puppet catalog;
+  # nothing is interpolated in these templates.
+  if $epel_gpg_managed {
     file { "/etc/pki/rpm-gpg/RPM-GPG-KEY-EPEL-${os_maj_release}":
       ensure  => present,
       owner   => 'root',
@@ -208,14 +229,16 @@ class epel (
 
     epel::rpm_gpg_key{ "EPEL-${os_maj_release}":
       path   => "/etc/pki/rpm-gpg/RPM-GPG-KEY-EPEL-${os_maj_release}",
-      before => Yumrepo['epel','epel-source','epel-debuginfo','epel-testing','epel-testing-source','epel-testing-debuginfo'],
     }
+  }
 
   } elsif "${::osfamily}" == 'RedHat' and "${::operatingsystem}" == 'Amazon' { # lint:ignore:only_variable_string
+  if $epel_managed {
     yumrepo { 'epel':
       enabled  => $epel_enabled,
       gpgcheck => $epel_gpgcheck,
     }
+  }
   } else {
     notice ("Your operating system ${::operatingsystem} will not have the EPEL repository applied")
   }
