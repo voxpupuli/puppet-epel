03d58dd2563558fc9b339fe6f9016a974795a8bd
Jeffrey Clark <jclark@nmi.com>
2017-04-23 10:51:42 -0500

    update url path to /metalink
    Same as default epel package config for > epel 5
    epel 5 package (EOL March 2017) still uses mirrorlist

--- manifests/params.pp
+++ manifests/params.pp
@@ -15,7 +15,13 @@ class epel::params {
     $os_maj_release = $os_versions[0]
   }
 
-  $epel_mirrorlist                        = "https://mirrors.fedoraproject.org/mirrorlist?repo=epel-${os_maj_release}&arch=\$basearch"
+  if versioncmp($os_maj_release, '5') > 0 {
+      $url_path = 'metalink'
+  } else {
+      $url_path = 'mirrorlist'
+  }
+
+  $epel_mirrorlist                        = "https://mirrors.fedoraproject.org/${url_path}?repo=epel-${os_maj_release}&arch=\$basearch"
   $epel_baseurl                           = 'absent'
   $epel_failovermethod                    = 'priority'
   $epel_proxy                             = $proxy
@@ -26,13 +32,13 @@ class epel::params {
   $epel_testing_proxy                     = $proxy
   $epel_testing_enabled                   = '0'
   $epel_testing_gpgcheck                  = '1'
-  $epel_source_mirrorlist                 = "https://mirrors.fedoraproject.org/mirrorlist?repo=epel-source-${os_maj_release}&arch=\$basearch"
+  $epel_source_mirrorlist                 = "https://mirrors.fedoraproject.org/${url_path}?repo=epel-source-${os_maj_release}&arch=\$basearch"
   $epel_source_baseurl                    = 'absent'
   $epel_source_failovermethod             = 'priority'
   $epel_source_proxy                      = $proxy
   $epel_source_enabled                    = '0'
   $epel_source_gpgcheck                   = '1'
-  $epel_debuginfo_mirrorlist              = "https://mirrors.fedoraproject.org/mirrorlist?repo=epel-debug-${os_maj_release}&arch=\$basearch"
+  $epel_debuginfo_mirrorlist              = "https://mirrors.fedoraproject.org/${url_path}?repo=epel-debug-${os_maj_release}&arch=\$basearch"
   $epel_debuginfo_baseurl                 = 'absent'
   $epel_debuginfo_failovermethod          = 'priority'
   $epel_debuginfo_proxy                   = $proxy
