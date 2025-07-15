d23b0e98782fcdc25f2dd58c3e8d4b0cf0a8edb4
Christoph Maser <christoph.maser+github@gmail.com>
2025-07-15 15:51:50 +0200

    test: fix scop issue, simplify test

--- spec/defines/rpm_gpg_key_spec.rb
+++ spec/defines/rpm_gpg_key_spec.rb
@@ -14,42 +14,16 @@ describe 'epel::rpm_gpg_key' do
   on_supported_os(test_on).each do |os, os_facts|
     context "on #{os}" do
       let(:facts) { os_facts }
-
-      case os_facts[:operatingsystemmajrelease]
-      when '7'
-        let :title do
-          'EPEL-7'
-        end
-        let :pre_condition do
-          'file { "/etc/pki/rpm-gpg/RPM-GPG-KEY-EPEL-7": }'
-        end
-        let :params do
-          { path: '/etc/pki/rpm-gpg/RPM-GPG-KEY-EPEL-7' }
-        end
-
-      when '8'
-        let :title do
-          'EPEL-8'
-        end
-        let :pre_condition do
-          'file { "/etc/pki/rpm-gpg/RPM-GPG-KEY-EPEL-8": }'
-        end
-        let :params do
-          { path: '/etc/pki/rpm-gpg/RPM-GPG-KEY-EPEL-8' }
-        end
-
-      when '9'
-        let :title do
-          'EPEL-9'
-        end
-        let :pre_condition do
-          'file { "/etc/pki/rpm-gpg/RPM-GPG-KEY-EPEL-9": }'
-        end
-        let :params do
-          { path: '/etc/pki/rpm-gpg/RPM-GPG-KEY-EPEL-9' }
-        end
-
+      let :title do
+        "EPEL-#{facts[:os]['release']['major']}"
       end
+      let :pre_condition do
+        "file { '/etc/pki/rpm-gpg/RPM-GPG-KEY-EPEL-#{facts[:os]['release']['major']}': }"
+      end
+      let :params do
+        { path: "/etc/pki/rpm-gpg/RPM-GPG-KEY-EPEL-#{facts[:os]['release']['major']}" }
+      end
+
       it do
         expect(subject).to contain_exec("import-#{title}").with(
           path: '/bin:/usr/bin:/sbin:/usr/sbin',
